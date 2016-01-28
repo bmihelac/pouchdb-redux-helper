822b81f3f7589ba720f5d04ddd3e2c33ea69c5ad
Bojan Mihelac <bmihelac@mihelac.org>
2016-01-28 19:18:58 +0100

    typo fix

--- src/crud/index.js
+++ src/crud/index.js
@@ -1,6 +1,6 @@
 export { default as createCRUD } from './crud';
 import * as containers from './containers';
-export { default as paginate } from './paginate';
+export { default as paginate } from './pagination';
 
 export {
   containers,
